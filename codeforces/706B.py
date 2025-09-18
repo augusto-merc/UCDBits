ade667b8a716b207048b2265d4540bde46d5e543
augusto-market <73326313+augusto-merc@users.noreply.github.com>
2025-09-18 00:40:09 +0000

    solve codeforces bug

--- codeforces/706B.py
+++ codeforces/706B.py
@@ -1,35 +1,14 @@
-def merge(arr, value, left, mid, right):
-    if abs(left - right) <= 1:
-        return mid
-    if value < arr[mid]:
-        return merge(arr, value, left, (left + mid) // 2, mid)
-    elif value >= arr[mid]:
-        return merge(arr, value, mid, (mid + right) // 2, right)
-    
-    ...
-    
+import bisect
 
 n = int(input())
-
 x = list(map(int, input().split()))
 x.sort()
-
 q = int(input())
+
 r = []
 
-for i in range(q):
+for _ in range(q):
     m = int(input())
-    
-    ind = merge(x, m, 0, n // 2, n - 1)
-    if ind == 0:
-        if m < x[0]:
-            r.append(0)
-        else:
-            r.append(1)
-    else:
-        r.append(ind+1)
-    
-    ...
+    r.append(bisect.bisect_right(x, m))
 
-    
 print(*r, sep='\n')
